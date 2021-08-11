445d63e50c6b1ac5e0adfec67f2918c0b42c9827
Hatab <elabdben@alum.us.es>
2021-08-11 20:48:25 +0200

    commit 5

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,6 +6,8 @@ import { withStyles } from '@material-ui/core/styles';
 import { AppBar, Toolbar, Typography } from '@material-ui/core';
 import AccessHeader from './AccessHeader';
 import Card from 'react-bootstrap/Card';
+import DateTime from './DateTime';
+import ProgressBar from 'react-bootstrap/ProgressBar';
 
 
 function NavBar(props) {
@@ -15,18 +17,18 @@ function NavBar(props) {
         <header>
             <Card.Header position="static"><AccessHeader/> </Card.Header>
             <AppBar className={classes.NavColor} position="static">
-                <Toolbar className="barra" variant="dense">
-                    <Typography variant="h6" component="p">Gestión de pacientes</Typography>
-                </Toolbar>
+                <div><ProgressBar variant="success" now ={100}/></div>
             </AppBar>
             <Navbar className="navbar-light" bg="light" variant="light">
                 <Nav className="mr-auto">
-                    <Navbar.Brand href="/datos"> <h6>Inicio</h6></Navbar.Brand>
-                    <Navbar.Brand href="/validacion"> <h6>Validación</h6></Navbar.Brand>
-                    <Navbar.Brand href="/resultados"> <h6>Resultados</h6></Navbar.Brand>
+                    <Navbar.Brand href="/datos"> <h6></h6></Navbar.Brand>
+                    <Navbar.Brand href="/datos"> <h5>Inicio</h5></Navbar.Brand>
+                    <Navbar.Brand href="/validacion"> <h5>Validación</h5></Navbar.Brand>
+                    
                 </Nav>
             </Navbar>
             </header>
+            
         </>
     )
 }
